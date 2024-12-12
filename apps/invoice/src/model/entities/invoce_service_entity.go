f5f9c97e9e58bc59056104bee51e231a80ec36eb
Lucas Montenegro <lucasmontenegro475@gmail.com>
2024-12-12 19:11:28 -0300

    chore: update json keys

--- apps/invoice/src/model/entities/invoce_service_entity.go
+++ apps/invoice/src/model/entities/invoce_service_entity.go
@@ -3,39 +3,39 @@ package entities
 import "time"
 
 type Invoice struct {
-	Id        int
-	Info      Info
-	Issuer    Person
-	Recipient Person
-	Service   Service
-	Total     float64
+	Id        int     `json:"id"`
+	Info      Info    `json:"info"`
+	Issuer    Person  `json:"issuer"`
+	Recipient Person  `json:"recipient"`
+	Service   Service `json:"service"`
+	Total     float64 `json:"total"`
 }
 
 type Info struct {
-	Key        string
-	Number     int
-	Competence time.Time
-	DateIssue  time.Time
-	Series     int
+	Key        string    `json:"key"`
+	Number     int       `json:"number"`
+	Competence time.Time `json:"competence"`
+	DateIssue  time.Time `json:"dateIssue"`
+	Series     int       `json:"series"`
 }
 
 type Person struct {
-	Name     string
-	Identity string
-	Address  Address
-	City     string
+	Name     string  `json:"name"`
+	Identity string  `json:"identity"`
+	Address  Address `json:"address"`
+	City     string  `json:"city"`
 }
 
 type Address struct {
-	Street       string
-	Number       string
-	Neighborhood string
-	Complement   string
+	Street       string `json:"street"`
+	Number       string `json:"number"`
+	Neighborhood string `json:"neighborhood"`
+	Complement   string `json:"complement"`
 }
 
 type Service struct {
-	Code               string
-	CodeDescription    string
-	ServiceDescription string
-	LocationProvision  string
+	Code               string `json:"code"`
+	CodeDescription    string `json:"codeDescription"`
+	ServiceDescription string `json:"serviceDescription"`
+	LocationProvision  string `json:"locationProvision"`
 }
